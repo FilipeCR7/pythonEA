4d208878445304422d4da3e764b1fc284a71b817
ricosantana <91482528+ricosantana@users.noreply.github.com>
2023-12-14 22:23:05 +0000

    trading execution logic added

--- api/oanda_api.py
+++ api/oanda_api.py
@@ -27,3 +27,46 @@ class OandaAPI:
         params = {'instruments': instruments}
         response = requests.get(url, headers=self.headers, params=params)
         return response.json()
+
+    def create_market_order(self, instrument, units):
+        url = f"{self.api_base_url}/accounts/{self.account_id}/orders"
+        data = {
+            "order": {
+                "instrument": instrument,
+                "units": units,
+                "type": "MARKET"
+            }
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        return response.json()
+
+    def create_limit_order(self, instrument, units, price):
+        url = f"{self.api_base_url}/accounts/{self.account_id}/orders"
+        data = {
+            "order": {
+                "instrument": instrument,
+                "units": units,
+                "price": str(price),
+                "type": "LIMIT"
+            }
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        return response.json()
+
+    def create_stop_order(self, instrument, units, price):
+        url = f"{self.api_base_url}/accounts/{self.account_id}/orders"
+        data = {
+            "order": {
+                "instrument": instrument,
+                "units": units,
+                "price": str(price),
+                "type": "STOP"
+            }
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        return response.json()
+
+    def close_trade(self, trade_id):
+        url = f"{self.api_base_url}/accounts/{self.account_id}/trades/{trade_id}/close"
+        response = requests.put(url, headers=self.headers)
+        return response.json()
--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def main():
 
     # Here, you can also initialize and execute your trading strategies,
     # perform data analysis, backtesting, etc.
+    # Initialization and existing code...
+
+    # Test creating a market order
+    oanda_api.create_market_order("USD_CAD", 1000)
+
+    # Test creating a limit order
+    oanda_api.create_limit_order("USD_CAD", 1000, 1.1500)
+
+    # Test creating a stop order
+    oanda_api.create_stop_order("USD_CAD", -1000, 1.1400)
 
 if __name__ == "__main__":
     main()
